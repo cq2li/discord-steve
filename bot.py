e7bdd6806d0c00fa7a5b3e7abed4c3c8bbe59f13
George <cqli1773@gmail.com>
2023-06-21 04:31:30 -0400

    Implemented discord commands and a daemon to scrape webs on adaptive random times On connection ready the bot launches the daemon as a separate thread to scrape leviatan scans and stores the data in a sqlite3 database.
    Many functionalities can be improved by generalizing functions and using
    a more robust database if needed.

--- bot.py
+++ bot.py
@@ -2,8 +2,10 @@ import discord
 from configparser import ConfigParser
 from pathlib import Path
 import lib as lib
-
-
+import threading
+import lib
+import asyncio
+import logging
 
 BASE = Path(__file__).resolve().parent
 CONFIG = ConfigParser()
@@ -14,14 +16,32 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+help_text = """
+1. '/refresh' to manually refresh the database of chapters
+2. '/latest' and '/last_checked' to request the latest chapter and last scrape time
+3. '/dm me' for good times😉
 
+"""
+
+async def send_msg(channel, msg):
+    await channel.send("@everyone")
+    await channel.send("hi")
+    await channel.send(msg)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    logging.info(f'We have logged in as {client.user}')
     if not hasattr(client, 'appinfo'):
         client.appinfo = await client.application_info()
-        print(client.appinfo)
+        # print(client.appinfo)
+    for gd in client.guilds:
+        for ch in gd.text_channels:
+            await ch.send(client.appinfo.description)
+            await ch.send(help_text)
+    loop = asyncio.get_running_loop() # has to use their event_loop to send messages
+    check_loop = threading.Thread(target = lib.checking_loop, args=[client, loop], daemon=True)
+    check_loop.start()
+            
     
 
 @client.event
@@ -32,10 +52,25 @@ async def on_message(message):
     if message.content == '/dm me':
         dm = await client.create_dm(message.author)
         await dm.send('suh duh')
+        await dm.send('good times')
 
-    if message.content == '/latest_chapter swordking':
-        newest_update = lib.swordking_latest("swordking")
+    if message.content == '/latest':
+        newest_update = lib.latest("swordking")
         await message.channel.send(newest_update)
 
+    if message.content == '/refresh':
+       await message.channel.send(lib.refresh())
+
+    if message.content == '/shutdown':
+        await client.close()
+
+    if message.content == '/help':
+        await message.channel.send(help_text)
+
+    if message.content == '/last_checked':
+        await message.channel.send("Last checked at " + lib.last_scrape())
+
+    if message.content == '/purge' and client.get_user(1117855078032879717):
+        await message.channel.purge(check = lambda x : True)
 
 client.run(BOT_KEY)
--- lib.py
+++ lib.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
+import datetime as dt
 from datetime import datetime
 import sqlite3
 from configparser import ConfigParser
-import asyncio
+import re
+import logging
+import time
+import discord
+import random
 
 BASE_DIR = Path(__file__).resolve().parent
 CONFIG = ConfigParser()
@@ -13,10 +18,11 @@ DB = 'Chapter_Releases.db'
 p = Path('./output')
 
 HEADERS = { 'Accept-Language': 'en',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
+'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
 ## ensure no spaces in NAME
 NAME = 'survival-story-of-a-sword-king-in-a-fantasy-world'
 URL = 'https://en.leviatanscans.com/manga/survival-story-of-a-sword-king-in-a-fantasy-world/'
+# URL = 'https://en.leviatanscans.com/manga/the-max-leveled-hero-will-return/'
 API = 'ajax/chapters'
 
 turl_API = 'https://api.tinyurl.com/create'
@@ -24,15 +30,26 @@ turl_PAYLOAD = { 'domain':'tinyurl.com' }
 turl_HEADERS = HEADERS.copy()
 turl_HEADERS['Authorization'] = f'Bearer {CONFIG.get("tinyurl", "secret")}'
 
-def connect(db):
-    con = sqlite3.connect(db)
-    return con, con.cursor()
+logging.basicConfig(
+        filename = 'lib.log', 
+        encoding = 'utf-8', 
+        level = logging.INFO,
+        format = '%(asctime)s %(message)s',
+        datefmt = '%m/%d/%Y %I:%M:%S %p',
+        )
+logging.info("Logging starting")
 
-def swordking_refresh():
+LAST_SCRAPE = None
+
+def connect(db: str):
+    con = sqlite3.connect(db, isolation_level= 'DEFERRED')
+    return con
+
+def refresh():
     '''
     Scrapes Leviatan scans for lastest chapters and updates the sqlite database
     '''
-    con, cur = connect(DB)
+    con = connect(DB)
     response = requests.post(url = URL + API, headers = HEADERS)
     parsed = BeautifulSoup(response.text, 'html.parser')
     ul = parsed.find('ul', class_='main')
@@ -40,24 +57,60 @@ def swordking_refresh():
     
     result = []
     display_name = NAME.replace('-', ' ').title()
+    
+    re_chapter = re.compile('Chapter (\d+)')
+    re_date = re.compile('(\d+) (day|hour|min|s)?')
 
     # clean release info
     for listing in ul:
         chapter = listing.a.string.strip()
+        try:
+            chapter_number = re_chapter.match(chapter).groups()[0]
+        except Exception as e:
+            logging.warning(f'Could not convert {chapter} to chapter number {e}')
+            continue
+
         link    = listing.a['href']
+
         release = listing.i.string.strip()
+
         try:
             release = datetime.strptime(release, '%B %d, %Y')
-        except:
-            release = datetime.today()
-        result.append((release, chapter, display_name, link))
+        except Exception as e:
+            logging.warning(e)
+            re_matched = re_date.match(release)
 
-    # store in database
+            if re_matched == None:
+                logging.error(f'{release} was not regex matched')    
+                release = datetime.today()
+            else:
+                delta, units = re_matched.groups()
+                delta = int(delta)
+                match units:
+                    case 'day':
+                        offset = dt.timedelta(days = -delta)
+                    case 'hour':
+                        offset = dt.timedelta(hours = -delta)
+                    case 'min':
+                        offset = dt.timedelta(minutes = -delta)
+                    case 's':
+                        offset = dt.timedelta(seconds = -delta)
+                    case _:
+                        logging.error(f'{units} was not accounted for in match')
+                        offset = dt.timedelta()
+                release = datetime.now() + offset
+                
+        result.append((release, chapter_number, display_name, link))
+
+    # store in database, lock
     with con:
         cur = con.cursor()
-        cur.execute(f'CREATE TABLE IF NOT EXISTS swordking(release, chapter, name, link, UNIQUE(chapter) ON CONFLICT REPLACE);')
+        cur.execute(f'CREATE TABLE IF NOT EXISTS swordking(release, chapter INTEGER, name, link, UNIQUE(chapter) ON CONFLICT REPLACE);')
         cur.executemany('INSERT INTO swordking VALUES(?, ?, ?, ?)', result)
         con.commit()
+    global LAST_SCRAPE
+    LAST_SCRAPE = datetime.now()
+    return f'Successful scrap of SwordKing'
 
 def notify(row):
     '''
@@ -71,7 +124,7 @@ def notify(row):
     turl = requests.post(turl_API, headers = turl_HEADERS, json = turl_PAYLOAD)
     tinyurl = turl.json()['data']
 
-    manga = f'{chapter} is the latest chapter of { manga_name.replace("-", " ").title() }, '
+    manga = f'Chapter {chapter} is the latest chapter of { manga_name.replace("-", " ").title() }, '
 
     ago = (datetime.now() - release).days
     ago_str = 'today' if ago == 0 else str(ago) + ' days ago'
@@ -82,53 +135,99 @@ def notify(row):
 
     created = f'(link created on { tinyurl_time }).'
 
-    print(manga + released + link + created)
+    # print(manga + released + link + created)
     return manga + released + link + created
 
-def swordking_has_new_updates(tablename):
+def has_new_updates(tablename: str):
     '''
     Return a boolean for whether a chapter has new releases
     tablename - the table name under which the manga is stored
     '''
-    con, cur = connect(DB)
-    cur.execute(f'CREATE TABLE IF NOT EXISTS latest(name TEXT, chapter, UNIQUE(name) ON CONFLICT REPLACE);')
-    last_checked = cur.execute(f'select * from latest where name = "{tablename}";').fetchall()
-    if last_checked:
-        last_checked = last_checked[0]
-    try:
-        latest = cur.execute(f'select * from {tablename} where release = (select max(release) from {tablename});').fetchall()
-        assert(len(latest) == 1)
-        latest = latest[0]
-    except Exception as e:
-        con.close()
-        print(e)
-        return False
-    if not latest:
-        con.close()
-        return False
-    elif last_checked and last_checked[1] == latest[1]:
-        con.close()
-        return False
-    else:
-        cur.execute('INSERT INTO latest VALUES(?, ?)', [tablename, latest[1]])
-        con.commit()
-        con.close()
-        return True
+    con = connect(DB)
+    with con:
+        cur = con.cursor()
+        cur.execute(f'CREATE TABLE IF NOT EXISTS latest(name TEXT, chapter INTEGER, UNIQUE(name) ON CONFLICT REPLACE);')
+        last_checked = cur.execute(f'select * from latest where name = "{tablename}";').fetchall()
+        if last_checked:
+            last_checked = last_checked[0]
+        try:
+            latest = cur.execute(f'select * from {tablename} where chapter = (select max(chapter) from {tablename});').fetchall()
+            assert(len(latest) == 1)
+            latest = latest[0]
+        except Exception as e:
+            print(e)
+            return False
+        if not latest:
+            return False
+        elif last_checked and last_checked[1] == latest[1]: # this checks max chapter
+            return False
+        else:
+            cur.execute('INSERT INTO latest VALUES(?, ?)', [tablename, latest[1]])
+            con.commit()
+            return True
 
-def swordking_latest(tablename):
+def latest(tablename: str):
     '''
     Returns the latest Chapter for a manga
     tablename - the table name under which the manga is stored
     '''
-    con, cur = connect(DB)
+    con = connect(DB)
     try:
-        latest = cur.execute(f'select * from {tablename} where release = (select max(release) from {tablename});').fetchall()
-        print(latest)
+        with con:
+            cur = con.cursor()
+            latest = cur.execute(f'select * from {tablename} where chapter = (select max(chapter) from {tablename});').fetchall()
+        # print(latest)
         assert(len(latest) == 1)
         latest = latest[0]
     except Exception as e:
         print(e)
-        con.close()
         return str(e)
-    con.close()
     return notify(latest)
+
+def _latest(tablename: str):
+    '''
+    Returns the latest Chapter for a manga
+    tablename - the table name under which the manga is stored
+    Internal usage for loop
+    '''
+    con = connect(DB)
+    try:
+        with con:
+            cur = con.cursor()
+            latest = cur.execute(f'select * from {tablename} where release = (select max(release) from {tablename});').fetchall()
+        assert(len(latest) == 1)
+        latest = latest[0]
+    except Exception as e:
+        print(e)
+        return str(e)
+    return latest[0]
+
+async def send_msg(channel, msg):
+    await channel.send(msg)
+
+def checking_loop(client: discord.Client, event_loop):
+    while True:
+        refresh()
+        if has_new_updates('swordking'):
+            channel = client.get_channel(1117858953750126744)
+            update = latest('swordking')
+            event_loop.create_task(channel.send('@everyone\n' + update))
+
+        last_update = datetime.strptime(_latest('swordking'), "%Y-%m-%d %H:%M:%S")
+        now = datetime.now()
+        elapsed = datetime.now() - last_update
+        logging.info(f'last checked at {now}')
+        logging.info("Days elapsed since last refresh: " +  str(elapsed.days))
+        if elapsed.days > 6:
+            # check once every 5 minutes
+            time.sleep(60 * 3 + random.uniform(1 * 60, 4 * 60))
+        else:
+            #check once every 4 hours
+            # time.sleep(60 * 60 * 4)
+            time.sleep(60 * 60 * 4 + random.uniform(60 * 60 * 1, 60 * 60 * 2))
+        
+
+def last_scrape():
+    if isinstance(LAST_SCRAPE, datetime):
+        return datetime.strftime(LAST_SCRAPE, "%Y-%m-%d %I:%M:%S %p %Z")
+    return "No time found"
