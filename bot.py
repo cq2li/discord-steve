f9ac460659b86eddf185435899d258da892d7e0e
George <cqli1773@gmail.com>
2023-06-24 18:02:03 -0400

    Add daemon thread restart
    Thread exceptions are now logged and automatically restarted up to 5
    times a day.
    
    Crashing more than 5 times in a day will stop the entire thread and
    display a discord message.

--- bot.py
+++ bot.py
@@ -1,11 +1,6 @@
-import discord
+import discord, lib, asyncio, logging, threading, const_texts, datetime as dt
 from configparser import ConfigParser
 from pathlib import Path
-import lib as lib
-import threading
-import lib
-import asyncio
-import logging
 
 BASE = Path(__file__).resolve().parent
 CONFIG = ConfigParser()
@@ -18,37 +13,63 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-help_text = """
-1. '/refresh' to manually refresh the database of chapters
-2. '/latest' and '/last_scraped' to request the latest chapter and last scrape time
-3. '/dm me' for good times😉
+THREADS = {}
 
-"""
-
-async def send_msg(channel, msg):
-    await channel.send("@everyone")
-    await channel.send("hi")
-    await channel.send(msg)
+def daemon_wrap(target):
+    '''
+    A decorator function that restarts the daemon threads upon crash
+    '''
+    def wrapper(*args):
+        counter = 5
+        counter_modified = dt.datetime.now()
+        thread_name = threading.current_thread().name
+        while True:
+            try:
+                logging.info(f'Trying to start {thread_name}\n')
+                target(*args)
+            except Exception as e:
+                # reset the counter if it's been more than 24 hours since the last crash
+                if counter < 5 and (dt.datetime.now() - counter_modified).days > 0:
+                    counter = 5
+                else:
+                    counter -= 1
+                logging.exception(f'{thread_name} raised {e}\n')
+                # stop the thread if it crashed more than 5 times in the last 24 hours
+                if counter == 0:
+                    logging.info(f'{thread_name} closing thread')
+                    break
+    return wrapper
 
 @client.event
 async def on_ready():
-    global SCRAPING_LOOP
+    '''
+    When the bot is running, initalise all the threads after this point to ensure
+    the event loop is running
+
+    '''
     logging.info(f'We have logged in as {client.user}')
+    
+    # retrieve the client application info for meta information
     if not hasattr(client, 'appinfo'):
         client.appinfo = await client.application_info()
-        # print(client.appinfo)
+    
+    # send help message texts
     for gd in client.guilds:
         for ch in gd.text_channels:
             await ch.send(client.appinfo.description)
-            await ch.send(help_text)
-    loop = asyncio.get_running_loop() # has to use their event_loop to send messages
-    SCRAPING_LOOP = threading.Thread(target = lib.checking_loop, args=[client, loop], daemon=True)
-    SCRAPING_LOOP.start()
+            await ch.send(const_texts.help_text)
+    
+    # initialise thread dictionary
+    THREADS["leviatanDaemon"] = threading.Thread(
+            target = daemon_wrap(lib.daemon_refresh),                          
+            args=[client, asyncio.get_running_loop()], 
+            daemon=True, 
+            name="leviathanDaemon")
+    THREADS["leviatanDaemon"].start()
             
     
 @client.event
 async def on_message(message):
-    global SCRAPING_LOOP
 
     if message.author == client.user:
         return
@@ -81,8 +102,8 @@ async def on_message(message):
         await message.channel.send(threading.enumerate())
 
     if message.content == '/check_scraper':
-        if SCRAPING_LOOP:
-            await message.channel.send("Running" if SCRAPING_LOOP.is_alive() else "Crashed")
+        if THREADS['leviatanDaemon']:
+            await message.channel.send("Running" if THREADS['leviatanDaemon'].is_alive() else "Crashed")
         else:
             await message.channel.send("Not initialised")
 
@@ -92,17 +113,5 @@ async def on_message(message):
     if message.content == '/set_verbose 1':
         lib.VERBOSE = 1
         await message.channel.send(f'VERBOSE set to {lib.VERBOSE}')
-    
-    if message.content == '/start_scrape':
-        if not SCRAPING_LOOP.is_alive():
-            try:
-                loop = asyncio.get_running_loop() # has to use their event_loop to send messages
-                SCRAPING_LOOP = threading.Thread(target = lib.checking_loop, args=[client, loop], daemon=True)
-                SCRAPING_LOOP.start()
-                await message.channel.send("Successfully restarted")
-            except Exception as e:
-                await message.channel.send(str(e))
-        else:
-            await message.channel.send("Already running")
 
 client.run(BOT_KEY)
